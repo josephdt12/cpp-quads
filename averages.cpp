4d6487d38439720f779733ef62405c390842380b
Joseph Thomas <josephdt12@gmail.com>
2017-03-05 22:42:09 -0800

    Implement fully working example

--- averages.cpp
+++ averages.cpp
@@ -2,7 +2,8 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <sstream>
 using namespace cv;
 
 class Quadrant {
@@ -10,14 +11,25 @@ public:
 	int top_x_, top_y_;
 	cv::Mat data_;
 	std::vector<double> means_;
+	double error_;
 
-	// Copy rectangle from region
+	// Construct a Quadrant from passed in region, and relate it to orig position
 	Quadrant(cv::Mat& region, int top_x, int top_y, int width, int height,
 			int orig_x, int orig_y) : means_(3,0) {
 
 		data_ = region(Rect(top_x, top_y, width, height));
 		top_x_ = orig_x + top_x;
 		top_y_ = orig_y + top_y;
+		error_ = 0;
+	}
+
+	// Basic copy constructor
+	Quadrant(const Quadrant& quad) {
+		top_x_ = quad.top_x_;
+		top_y_ = quad.top_y_;
+		data_ = quad.data_;
+		means_ = quad.means_;
+		error_ = quad.error_;
 	}
 
 	// Divide Quadrant into 4 Quadrants and return
@@ -58,45 +70,69 @@ public:
 	void FillRegion() {
 		data_ = Scalar(means_[0], means_[1], means_[2]);
 	}
+
+	// Calculates the mean squared error compared to passed in values
+	double MeanSquareError(std::vector<double> vals) {
+		double errors[3];
+		for (int row = 0; row < data_.rows; ++row) {
+			for (int col = 0; col < data_.cols; ++col) {
+				cv::Vec3b point = data_.at<Vec3b>(row,col);
+				errors[0] += std::pow((point[0] - vals[0]),2);
+				errors[1] += std::pow((point[1] - vals[1]),2);
+				errors[2] += std::pow((point[2] - vals[0]),2);
+			}
+		}
+
+		error_ = ((errors[0] + errors[1] + errors[2]) / 3);
+		return error_;
+	}
 };
 
-// Determine the mean square error for a region (R,G, and B combined)
-double MeanSquareError(cv::Mat& region, std::vector<double>& vals) {
-	double errors[3];
-	for (int row = 0; row < region.rows; ++row) {
-		for (int col = 0; col < region.cols; ++col) {
-			cv::Vec3b point = region.at<Vec3b>(row,col);
-			errors[0] += std::pow((vals[0] - point[0]),2);
-			errors[1] += std::pow((vals[1] - point[1]),2);
-			errors[2] += std::pow((vals[2] - point[0]),2);
-		}
-	}
-
-	return (errors[0] + errors[1] + errors[2]) / 3;
-}
-
-void FillRegion(cv::Mat& region, std::vector<double>& RGB) {
-	region = Scalar(RGB[0], RGB[1], RGB[2]);
+bool operator<(const Quadrant& lhs, const Quadrant& rhs) {
+	return lhs.error_ < rhs.error_;
 }
 
 int main(int argc, char** argv) {
+	// Read in the image
 	cv::Mat image = cv::imread(argv[1]);
 	cv::Mat new_image = image.clone();
 
-	Quadrant original(new_image, 0, 0, new_image.cols, new_image.rows, 0, 0);
-	std::vector<Quadrant> quadrants = original.DivideRegion();
-
-	for (int i = 0; i < 4; ++i) {
-		std::vector<double> values = quadrants[i].RegionMean(image);
-		quadrants[i].FillRegion();
+	// Grab number of iterations from user, else default to 2048
+	int MAX_ITERATIONS;
+	if (argc < 3) {
+		MAX_ITERATIONS = 2048;
+	} else {
+		std::istringstream ss(argv[2]);
+		if (!(ss >> MAX_ITERATIONS)) {
+			std::cerr << "Invalid number " << argv[2] << std::endl;
+			return 1;
+		}
 	}
 
+	// Priority queue for tracking the largest error quadrant
+	std::priority_queue<Quadrant> min_heap;
+	Quadrant original(new_image, 0, 0, new_image.cols, new_image.rows, 0, 0);
+	Quadrant curr = original;
 
-	std::vector<Quadrant> quads2 = quadrants[3].DivideRegion();
-	std::vector<double> values = quads2[1].RegionMean(image);
-	quads2[1].FillRegion();
-	std::vector<double> values2 = quads2[3].RegionMean(image);
-	quads2[3].FillRegion();
+	for (int itr = 0; itr < MAX_ITERATIONS; ++itr) {
+		// Get current quadrant's mean values for sub-quadrants error calculation
+		std::vector<double> values = curr.RegionMean(image);
+
+		// Divide current quadrant into 4
+		std::vector<Quadrant> quadrants = curr.DivideRegion();
+
+		// Obtain the mean of each quadrant's position in the orig. image and fill
+		// Also place each quadrant into the priority_queue
+		for (int i = 0; i < 4; ++i) {
+			quadrants[i].MeanSquareError(values);
+			min_heap.push(quadrants[i]);
+			quadrants[i].RegionMean(image);
+			quadrants[i].FillRegion();
+		}
+
+		curr = min_heap.top();
+		min_heap.pop();
+	}
 
 	namedWindow("Original", CV_WINDOW_AUTOSIZE);
 	namedWindow("Quadrants", CV_WINDOW_AUTOSIZE);
